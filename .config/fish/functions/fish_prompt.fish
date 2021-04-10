1f767d1f68250babc806592f67ea81a6e545e1ff
James Hollister <j_hollister@jamh.me>
2021-04-09 23:58:22 -0700

    servers need a different hostname and tmux prefix
    When I'm logged into a server I like to be able to see that I'm on
    the server easily. I print the hostname on each prompt to make it
    clear.
    
    I also need a different prefix for tmux and chose Ctrl-s for now.
    
    In the future I can try to set up a way to do this without creating
    a new branch.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,8 +1,12 @@
-# Defined interactively
+# Defined in /tmp/fish.bPLUT4/fish_prompt.fish @ line 2
 function fish_prompt --description 'Write out the prompt'
     set -l last_pipestatus $pipestatus
     set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
-
+    set -q __fish_prompt_hostname
+    # Just calculate these once, to save a few cycles when displaying the prompt
+    if not set -q __fish_prompt_hostname
+        set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+    end
     if not set -q __fish_git_prompt_show_informative_status
         set -g __fish_git_prompt_show_informative_status 1
     end
@@ -69,12 +73,21 @@ function fish_prompt --description 'Write out the prompt'
         set suffix '$'
     end
 
+    # Print hostname
+    # Just calculate these once, to save a few cycles when displaying the prompt
+    if not set -q __fish_prompt_hostname
+        set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+    end
+
+    printf '%s ' (prompt_hostname)
+
     # PWD
     set_color $color_cwd
     echo -n (prompt_pwd)
     set_color normal
 
-    printf '%s ' (fish_vcs_prompt)
+
+    printf '%s '  (fish_vcs_prompt)
 
     set -l pipestatus_string (__fish_print_pipestatus "[" "] " "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)
     echo -n $pipestatus_string
