6a93b4cfad85a112c00bc0dd97ba6fd01961905b
James Hollister <j_hollister@jamh.me>
2021-04-25 21:02:31 -0700

    Use exa for ll and ls but fallback if not installed

--- .config/fish/functions/ll.fish
+++ .config/fish/functions/ll.fish
@@ -3,6 +3,7 @@ function ll --wraps='exa -l' --description 'alias ll=exa -l'
     if type exa &> /dev/null
         exa -l $argv; 
     else
+        # fallback to ls
         command ls -l $argv; 
     end
 end
